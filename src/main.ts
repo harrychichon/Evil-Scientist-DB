dccaf3004611f77c04d68e0291da3ab617e715a3
Harry <harry.chichon@proton.me>
2024-12-12 13:01:44 +0100

    Cleaned up Vite stuff

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Evil Scientist DB</h1>
+    <p class="read-the-docs">
+      Moahahahaha
+    </p>
+  </div>
+`
+
